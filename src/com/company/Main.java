f5af0e0de2b1b7934940f724a3525247f82c6d5d
HS211777 <HS211777@reigate.ac.uk>
2021-09-27 10:16:25 +0100

    branch1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,27 @@ public class Main {
     public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
     Random rn = new Random();
-    System.out.println("First Random int is "+rn.nextInt(10));
-    System.out.println("First Random double is "+rn.nextDouble());
+    //System.out.println("First Random int is "+rn.nextInt(10));
+    //System.out.println("First Random double is "+rn.nextDouble());
 
-    int min = 3;
-    int max = 5;
-    int newRandom = rn.nextInt((max-min)+1)+min;
-    System.out.println(newRandom);
+    //int min = 3;
+    //int max = 5;
+    //int newRandom = rn.nextInt((max-min)+1)+min;
+    //System.out.println(newRandom);
+        System.out.println("How many numbers?");
+        int userInput = input.nextInt();
+        System.out.println("Enter maximum number");
+        int max = input.nextInt();
+        System.out.println("Enter minimum number");
+        int min = input.nextInt();
+        if (min > max){
+            System.out.println("Error");
+        }
+        else{
+            for(int i = 0; i < userInput; i++){
+                System.out.println(rn.nextInt((max-min)+1)+min);
+            }
+        }
+
     }
 }
